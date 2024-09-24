5576890350cb039ff57e337a96323ed86b90e540
RulH19 <nurulh298hd@gmail.com>
2024-09-24 11:38:27 +0700

    menambah animasi hamburger

--- src/components/Sidebar/links/Links.jsx
+++ src/components/Sidebar/links/Links.jsx
@@ -1,13 +1,37 @@
+import { motion } from 'framer-motion';
+const variants = {
+  open: {
+    transition: {
+      staggerChildren: 0.1,
+    },
+  },
+  closed: {
+    transition: {
+      staggerChildren: 0.05,
+      staggerDirection: -1,
+    },
+  },
+};
+const itemVariants = {
+  open: {
+    y: 0,
+    opacity: 1,
+  },
+  closed: {
+    y: 100,
+    opacity: 0,
+  },
+};
 export const Links = () => {
   const items = ['Homepage', 'Services', 'Portofolio', 'Contact', 'About'];
 
   return (
-    <div className="links">
+    <motion.div className="links" variants={variants}>
       {items.map((item) => (
-        <a href={`#${item}`} key={item}>
+        <motion.a href={`#${item}`} key={item} variants={itemVariants} whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.95 }}>
           {item}
-        </a>
+        </motion.a>
       ))}
-    </div>
+    </motion.div>
   );
 };
